b6281feb5df14d4c5fb6858a0ebbd80650abf749
gblackhorn <guoda.gd@gmail.com>
2023-04-26 14:54:32 +0900

    pseudo code ver.1

--- SeeNeurons.m
+++ SeeNeurons.m
@@ -1,30 +1,39 @@
 function [neuronsList,varargout] = SeeNeurons(binaryMatrix,varargin)
 	% This function finds out neurons in a given binary image based on
-	% diameter threshold (diaThreshold) and circularity threshold (cirThreshold)
+	% diameter threshold (minDiameter) and circularity threshold (minCircularity)
 
 	% Example:
 	%		
 
 	% Defaults
-	diaThreshold = 40; % unit: pixel
-	cirThreshold = 0.6; % ranges from 0 (for a completely irregular shape) to 1 (for a perfect circle)
+	minDiameter = 40; % unit: pixel
+	maxDiameter = 40; % unit: pixel
+	minCircularity = 0.6; % ranges from 0 (for a completely irregular shape) to 1 (for a perfect circle)
+	noiseSigSizeScale = 2; % The max size of noise signal (connected area of ones) is minArea (calculated by minDiameter) devided by "noiseSigSizeScale" 
 
 	% Optionals
 	for ii = 1:2:(nargin-1)
-	    if strcmpi('diaThreshold', varargin{ii}) % trace mean value comparison (stim vs non stim). output of stim_effect_compare_trace_mean_alltrial
-	        diaThreshold = varargin{ii+1}; % normalize every FluoroData trace with its max value
-	    elseif strcmpi('cirThreshold', varargin{ii})
-            cirThreshold = varargin{ii+1};
+	    if strcmpi('minDiameter', varargin{ii}) % trace mean value comparison (stim vs non stim). output of stim_effect_compare_trace_mean_alltrial
+	        minDiameter = varargin{ii+1}; % normalize every FluoroData trace with its max value
+	    elseif strcmpi('maxDiameter', varargin{ii})
+            maxDiameter = varargin{ii+1};
+	    elseif strcmpi('minCircularity', varargin{ii})
+            minCircularity = varargin{ii+1};
+	    elseif strcmpi('noiseSigSizeScale', varargin{ii})
+            noiseSigSizeScale = varargin{ii+1};
 	    end
 	end
 
+	% Decide the minSize using the minDiameter
+	minArea = pi*(minDiameter/2)^2;
 
-	% 1. Decrease the noise in the image
-	% Open a GUI to display the binary Image Use the slider in the GUI to set
-	% the threshold for noise size. Update the binary Image when threshold is
-	% changed.
-	% Use function "bwareaopen"
-	bmDenoised = bwareaopen(bm, minSize); % get the de-noixed binaryMatrix 
+
+	% 1. Decrease the noise in the image with function "bwareaopen"
+	% Optional: Open a GUI to display the binary Image Use the
+	% slider in the GUI to set the threshold for noise size. Update the binary Image when threshold
+	% is changed. 
+	minSize = minArea/noiseSigSizeScale; 
+	bmDenoised = bwareaopen(binaryMatrix, minSize); % get the de-noixed binaryMatrix 
 
 
 	% 2. Get the properties of connected regions
@@ -33,9 +42,18 @@ function [neuronsList,varargout] = SeeNeurons(binaryMatrix,varargin)
 
 
 	% 3. Filter neurons in the list 
-	isNeuron = [stats.Area] >= minArea & [stats.EquivDiameter] >= minDiameter & [stats.Circularity] >= minCircularity;
+	neuronsList = [stats.EquivDiameter] >= minDiameter & [stats.EquivDiameter] <= maxDiameter & [stats.Circularity] >= minCircularity & [stats.Area] <= maxDiameter;
 
 
-	% 4. Create a binaryMatrix only contains the "isNeuron"
+	% 4. Calculate the mean, standard error, median, and normality for summary
+
+
+	% 5. Plot histogram for Area, EquivDiameter, Circularity, MajorAxisLength, and MinorAxisLength
+
+
+	% 6. Scatter plot of Area/Diameter vs circularity
+
+
+	% 7. Create a binaryMatrix only contains the "isNeuron"
 	bwNeurons = bwselect(bw, find(isNeuron));
 end
